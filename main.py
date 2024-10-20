b4f701c330d16de729b37ea804e43fbb212e6c56
Sanderson Bergmann <sander.bgm@gmail.com>
2024-10-20 20:03:10 -0300

    adicionado função de coleta de dados

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+def gether_data():
+    n1 = int(input("Digite o primeiro valor: "))
+    n2 = int(input("Digite o segundo valor: "))
+
+    return n1, n2
+
+
 def main():
-    print(2*2)
+    n1,n2 = gether_data()
+
+    print(n1,n2)
+
     return None
 
 if __name__ == "__main__":
